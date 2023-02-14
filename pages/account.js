5313f708f4dd4a9df16e0bac6d4b25337240a986
Oleksandr <boltbright525@gmail.com>
2023-02-14 16:55:14 +0200

    Re-style the account.js page

--- pages/account.js
+++ pages/account.js
@@ -1,24 +1,34 @@
 import Layout from "../components/Layout";
 import styles from "../styles/Account.module.css"
 import ProtectedRoute from "../components/ProtectedRoute";
+import { useAuth } from "./Contexts/AuthContext";
 
 const Account= ()=>{
+
+    const{currentUser}= useAuth()
+
     return(
     <Layout>
         <ProtectedRoute>
             <div className={styles.container}>
                 <form className={styles.personalDetails}>
                     <span className={styles.formTitle}>Personal Details</span>
-                    <input type="text" name="userName" id="" placeholder="Username" />
-                    <input type="text" name="fullName" id="" placeholder="Full Name" />
-                    <input type="text" name="email" id=""  placeholder="Email"/>
-                    <input type="tel" name="PhoneNumber" id=""  placeholder="Phone number"/>
-                    <input type="text" name="address" id=""  placeholder="Address"/>
+                    <label>Username</label>
+                    <input type="text" name="userName" id=""/>
+                    <label>Full Name</label>
+                    <input type="text" name="fullName" id="" />
+                    <label>Email</label>
+                    <input type="text" name="email" id="" defaultValue={currentUser.email}/>
+                    <label>Phone number</label>
+                    <input type="tel" name="PhoneNumber" id=""/>
+                    <label>Address</label>
+                    <input type="text" name="address" id=""/>
                 </form>
 
                 <form className={styles.security}>
                     <span className={styles.formTitle}>Security</span>
-                    <input type="password" name="password" id="" placeholder="Password" />
+                    <label>Password</label>
+                    <input type="password" name="password" id="" placeholder="Leave blank to keep the same"/>
                 </form>
             </div>
         </ProtectedRoute>
