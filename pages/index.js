5a3ebf7bc60e7831cb6ab363013fb6b63d40014a
Oleksandr <boltbright525@gmail.com>
2024-05-27 18:06:59 +0200

    updated index.js data fetching

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,15 @@
 import Head from "next/head";
 import Layout from "../components/Layout";
 import Hero from "../components/Hero";
+import styles from "../styles/Home.module.css";
 import Services from "../components/Services";
 import Menu from "../components/Menu";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import banner1 from "../assets/banner1.png";
+import banner2 from "../assets/banner2.png";
 import { url } from "../constants/constants";
-import styles from "../styles/Home.module.css";
 
-export default function Home() {
-  const [food, setFood] = useState([]);
-  const [foodCategories, setFoodCategories] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const [foodResponse, foodCategoryResponse] = await Promise.all([
-          axios.get(`${url}/api/admin/products`),
-          axios.get(`${url}/api/admin/categories`),
-        ]);
-
-        setFood(foodResponse.data);
-        setFoodCategories(foodCategoryResponse.data);
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error}</p>;
-
+export default function Home({ food, foodCategories }) {
   return (
     <Layout>
       <div className={styles.container}>
@@ -56,3 +29,28 @@ export default function Home() {
     </Layout>
   );
 }
+
+export const getServerSideProps = async () => {
+  try {
+    const foodResponse = await axios.get(`${url}/api/admin/products`);
+    const food = foodResponse.data;
+
+    const foodCategoryResponse = await axios.get(`${url}/api/admin/categories`);
+    const foodCategories = foodCategoryResponse.data;
+
+    return {
+      props: {
+        food,
+        foodCategories,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return {
+      props: {
+        food: [],
+        foodCategories: [],
+      },
+    };
+  }
+};
